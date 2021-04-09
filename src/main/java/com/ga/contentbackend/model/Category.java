01f9c4482073f0bb664f279aab5eeb4090295414
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-04-09 15:44:27 -0500

    Created fields, getters, setters, and constructors for category model

--- src/main/java/com/ga/contentbackend/model/Category.java
+++ src/main/java/com/ga/contentbackend/model/Category.java
@@ -2,4 +2,36 @@ package com.ga.contentbackend.model;
 
 public class Category {
 
+    private Long id;
+    private String title;
+    private String description;
+
+    public Category(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
