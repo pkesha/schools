45a133c6a74fd18f8d18f55b91863a26e4fc5a2f
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-04-09 15:49:51 -0500

    PK-Created fields, getters, setters, and constructors for user model

--- src/main/java/com/ga/contentbackend/model/User.java
+++ src/main/java/com/ga/contentbackend/model/User.java
@@ -2,4 +2,35 @@ package com.ga.contentbackend.model;
 
 public class User {
     private Long id;
+    private String email;
+    private String password;
+
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
