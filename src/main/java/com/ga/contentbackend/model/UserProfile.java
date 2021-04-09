8d39f299881b6524f323098f6757dd9ddf2bc0a5
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-04-09 15:52:13 -0500

    PK-Created fields, getters, setters, and constructors for user profile model

--- src/main/java/com/ga/contentbackend/model/UserProfile.java
+++ src/main/java/com/ga/contentbackend/model/UserProfile.java
@@ -1,4 +1,54 @@
 package com.ga.contentbackend.model;
 
+import java.util.List;
+
 public class UserProfile {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private List<String> credentials;
+
+    public UserProfile(Long id, String firstName, String lastName, List<String> credentials) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.credentials = credentials;
+    }
+
+    public UserProfile() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<String> getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(List<String> credentials) {
+        this.credentials = credentials;
+    }
 }
