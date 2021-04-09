b18375085d2e0714e50ac232ecfde49d6354fa7a
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-09 16:39:42 -0500

    added category controller

---
+++ src/main/java/com/ga/contentbackend/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package com.ga.contentbackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api")
+public class CategoryController {
+    /***************Categories**************/
+
+    public void getCategories(){
+
+    }
+
+    public void getCategory(){
+
+    }
+
+
+
+    /***************Review**************/
+
+
+
+    /***************Comments**************/
+}
--- src/main/java/com/ga/contentbackend/controller/HelloWorldController.java
+++ src/main/java/com/ga/contentbackend/controller/HelloWorldController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+//localhost:9092/categories/{categoryId}/reviews/{reviewId}/comments/{commentId}
+
     @GetMapping(path="/hello")
     public String getHello(){
         return "Hello World";
