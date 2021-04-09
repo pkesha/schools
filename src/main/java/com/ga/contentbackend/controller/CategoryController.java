90f43863829c82f19b81fea5d7a445f266144e12
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-09 16:46:41 -0500

    completed scaffold of Category Controller

--- src/main/java/com/ga/contentbackend/controller/CategoryController.java
+++ src/main/java/com/ga/contentbackend/controller/CategoryController.java
@@ -16,11 +16,63 @@ public class CategoryController {
 
     }
 
+    public void createCategory(){
 
+    }
+
+    public void updateCategory(){
+
+    }
+
+    public void deleteCategory(){
+
+    }
 
     /***************Review**************/
 
+    public void getCategoryReviews(){
+
+    }
+
+    public void getCategoryReview(){
+
+    }
+
+    public void createCategoryReview(){
+
+    }
+
+    public void updateCategoryReview(){
+
+    }
+
+    public void deleteCategoryReview(){
+
+    }
 
 
     /***************Comments**************/
+
+    public void getCategoryReviewComments(){
+
+    }
+
+    public void getCategoryReviewComment(){
+
+    }
+
+    public void createCategoryReviewComment(){
+
+    }
+
+    public void updateCategoryReviewComment(){
+
+
+    }
+
+    public void deleteCategoryReviewComment(){
+
+    }
+
+
 }
