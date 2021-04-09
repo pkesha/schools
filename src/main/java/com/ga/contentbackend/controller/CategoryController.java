95a250d15a842d473f8a039ea307f66544dba513
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-09 17:00:17 -0500

    category services updated with methods

--- src/main/java/com/ga/contentbackend/controller/CategoryController.java
+++ src/main/java/com/ga/contentbackend/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.ga.contentbackend.controller;
 
+import com.ga.contentbackend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,70 +10,78 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     /***************Categories**************/
 
-    public void getCategories(){
 
+    CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    public void getCategories(){
+        categoryService.getCategories();
     }
 
     public void getCategory(){
-
+        categoryService.getCategory();
     }
 
     public void createCategory(){
-
+        categoryService.createCategory();
     }
 
     public void updateCategory(){
-
+        categoryService.updateCategory();
     }
 
     public void deleteCategory(){
-
+        categoryService.deleteCategory();
     }
 
     /***************Review**************/
 
     public void getCategoryReviews(){
-
+        categoryService.getCategoryReviews();
     }
 
     public void getCategoryReview(){
-
+        categoryService.getCategoryReview();
     }
 
     public void createCategoryReview(){
-
+        categoryService.createCategoryReview();
     }
 
     public void updateCategoryReview(){
-
+    categoryService.updateCategoryReview();
     }
 
     public void deleteCategoryReview(){
-
+        categoryService.deleteCategoryReview();
     }
 
 
     /***************Comments**************/
 
     public void getCategoryReviewComments(){
-
+    categoryService.getCategoryReviewComments();
     }
 
     public void getCategoryReviewComment(){
-
+    categoryService.getCategoryReviewComment();
     }
 
     public void createCategoryReviewComment(){
-
+        categoryService.createCategoryReviewComment();
     }
 
     public void updateCategoryReviewComment(){
-
+    categoryService.updateCategoryReviewComment();
 
     }
 
     public void deleteCategoryReviewComment(){
-
+    categoryService.deleteCategoryReviewComment();
     }
 
 
--- src/main/java/com/ga/contentbackend/service/CategoryService.java
+++ src/main/java/com/ga/contentbackend/service/CategoryService.java
@@ -1,4 +1,75 @@
 package com.ga.contentbackend.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
+    /***************Categories**************/
+
+    public void getCategories(){
+
+    }
+
+    public void getCategory(){
+
+    }
+
+    public void createCategory(){
+
+    }
+
+    public void updateCategory(){
+
+    }
+
+    public void deleteCategory(){
+
+    }
+
+    /***************Review**************/
+
+    public void getCategoryReviews(){
+
+    }
+
+    public void getCategoryReview(){
+
+    }
+
+    public void createCategoryReview(){
+
+    }
+
+    public void updateCategoryReview(){
+
+    }
+
+    public void deleteCategoryReview(){
+
+    }
+
+
+    /***************Comments**************/
+
+    public void getCategoryReviewComments(){
+
+    }
+
+    public void getCategoryReviewComment(){
+
+    }
+
+    public void createCategoryReviewComment(){
+
+    }
+
+    public void updateCategoryReviewComment(){
+
+
+    }
+
+    public void deleteCategoryReviewComment(){
+
+    }
+
 }
