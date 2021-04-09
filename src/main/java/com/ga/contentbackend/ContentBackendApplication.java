117f30711320954a40020f9321d8234704d1c54b
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-09 15:28:06 -0500

    initial commit

---
+++ src/main/java/com/ga/contentbackend/ContentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ga.contentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ga/contentbackend/ContentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.contentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
