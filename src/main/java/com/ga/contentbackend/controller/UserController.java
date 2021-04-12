4711e7a89b896ef51a88983934b721be0f975fec
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-12 13:54:49 -0500

    added User Repository

---
+++ src/main/java/com/ga/contentbackend/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.ga.contentbackend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+
+}
