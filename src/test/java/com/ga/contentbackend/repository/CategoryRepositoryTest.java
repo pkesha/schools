f3c03d5664eee080fef6e5741c84e8c7b69c09ec
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-04-15 10:45:16 -0500

    testing for category with user id

--- src/test/java/com/ga/contentbackend/repository/CategoryRepositoryTest.java
+++ src/test/java/com/ga/contentbackend/repository/CategoryRepositoryTest.java
@@ -3,6 +3,8 @@ package com.ga.contentbackend.repository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ga.contentbackend.model.Category;
+import com.ga.contentbackend.model.User;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,10 +22,19 @@ class CategoryRepositoryTest {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     private Category category;
+    private User user;
+
     @BeforeEach
     public void setUp() {
         category = new Category(1L,"Movie","Movies and More");
+        user = new User(1L, "pk@gmail.com", "123456");
+        userRepository.save(user);
+        this.category.setUser(user);
     }
     @AfterEach
     public void tearDown() {
@@ -41,16 +52,21 @@ class CategoryRepositoryTest {
     @Test
     void givenSavedCategoryShouldReturnCategory() throws Exception {
         categoryRepository.save(category);
-        Category fetchedCategory =
-                categoryRepository.findById(category.getId()).get();
+        Category fetchedCategory = categoryRepository.findById(category.getId()).get();
         assertEquals(mapToJson(category), mapToJson(fetchedCategory));
     }
 
     @Test
-    void findByIdAndUserId() {
+    void givenSavedCategoryShouldReturnCategoryWithSameId() {
+        categoryRepository.save(category);
+        Category fetchedCategory = categoryRepository.findByIdAndUserId(category.getId(), this.user.getId());
+        assertEquals(category.getId(), fetchedCategory.getId());
+        assertEquals(category.getUser().getId(), fetchedCategory.getUser().getId());
     }
 
     @Test
     void findAllByUserId() {
     }
+
+
 }
