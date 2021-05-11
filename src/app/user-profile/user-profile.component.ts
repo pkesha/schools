6890ac7ae7b45b0bab72d794e692274f3201f067
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-11 16:26:28 -0500

    updated user-profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  public firstName;
+  public lastName;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  registerUserProfile(){
+    const userProfile = {
+      firstName: this.firstName,
+      lastName: this.lastName
+    }
+
+    this.userService.createUserProfile(userProfile);
+  }
 }
